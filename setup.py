548d265099835a923345e1a30d71d6c8c7d48374
Doug Turnbull <softwaredoug@gmail.com>
2023-12-23 07:15:03 -0500

    Remove scipy from setup.py

--- setup.py
+++ setup.py
@@ -126,7 +126,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/discussions/install-requires-vs-requirements/
-    install_requires=["pandas>=2.0.0", "scipy>=1.0.0", "sortednp"],  # Optional
+    install_requires=["pandas>=2.0.0", "sortednp"],  # Optional
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
     # syntax, for example:
