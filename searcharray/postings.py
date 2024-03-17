Fix BM25 idf calculation

searcharray/postings.py
@@ -222,6 +222,7 @@ class SearchArray(ExtensionArray):
         self.term_mat, self.posns, \
             self.term_dict, self.avg_doc_length, \
             self.doc_lens = build_index_from_terms_list(postings, Terms)
+        self.corpus_size = len(self.doc_lens)
 
     @classmethod
     def index(cls, array: Iterable,
@@ -267,6 +268,7 @@ class SearchArray(ExtensionArray):
         postings.term_dict = term_dict
         postings.avg_doc_length = avg_doc_length
         postings.doc_lens = doc_lens
+        postings.corpus_size = len(doc_lens)
         return postings
 
     def warm(self):
@@ -313,13 +315,18 @@ class SearchArray(ExtensionArray):
         else:
             # Construct a sliced view of this array
             sliced_tfs = self.term_mat.slice(key)
-            sliced_posns = self.posns.slice(sliced_tfs.rows) if not self.avoid_copies else self.posns
+            if not self.avoid_copies:
+                sliced_posns = self.posns.slice(sliced_tfs.rows) if not self.avoid_copies else self.posns
+            else:
+                sliced_posns = self.posns
+
             arr = SearchArray([], tokenizer=self.tokenizer)
             arr.term_mat = sliced_tfs
             arr.doc_lens = self.doc_lens[key]
             arr.posns = sliced_posns
             arr.term_dict = self.term_dict
             arr.avg_doc_length = self.avg_doc_length
+            arr.corpus_size = self.corpus_size
             return arr
 
     def __setitem__(self, key, value):
@@ -488,8 +495,10 @@ class SearchArray(ExtensionArray):
         postings_arr.term_dict = self.term_dict
         postings_arr.avg_doc_length = self.avg_doc_length
         postings_arr.scoring_context = self.scoring_context
+        postings_arr.corpus_size = self.corpus_size
 
         if not self.avoid_copies:
+            postings_arr.scoring_context = None
             postings_arr.posns = self.posns.copy()
             postings_arr.term_dict = self.term_dict.copy()
         return postings_arr
@@ -591,14 +600,14 @@ class SearchArray(ExtensionArray):
         token = self._check_token_arg(token)
         doc_lens = self.doclengths()
 
-        context = ScoringContext(doc_freqs=all_dfs,
-                                 doc_lens=doc_lens, avg_doc_lens=self.avg_doc_length,
-                                 num_docs=len(self))
+        context = ScoringContext(doc_lens=doc_lens,
+                                 avg_doc_lens=self.avg_doc_length,
+                                 num_docs=self.corpus_size)
 
         if not context.same_as(self.scoring_context):
             self.scoring_context = context
 
-        scores = similarity(tfs, self.scoring_context)
+        scores = similarity(tfs, all_dfs, self.scoring_context)
         return scores
 
     def positions(self, token: str, key=None) -> List[np.ndarray]:

searcharray/similarity.py
@@ -12,10 +12,8 @@ class ScoringContext:
 
     """
 
-    def __init__(self, doc_freqs: np.ndarray,
-                 doc_lens: np.ndarray, avg_doc_lens: int, num_docs: int):
+    def __init__(self, doc_lens: np.ndarray, avg_doc_lens: int, num_docs: int):
         """Initialize."""
-        self.doc_freqs = doc_freqs
         self.doc_lens = doc_lens
         self.avg_doc_lens = avg_doc_lens
         self.num_docs = num_docs
@@ -33,14 +31,15 @@ class ScoringContext:
 class Similarity(Protocol):
     """Similarity function protocol."""
 
-    def __call__(self, term_freqs: np.ndarray, context: ScoringContext) -> np.ndarray:
+    def __call__(self, term_freqs: np.ndarray, doc_freqs: np.ndarray,
+                 context: ScoringContext) -> np.ndarray:
         """Calculate similarity scores."""
         ...
 
 
-def compute_idf(num_docs, sum_dfs):
+def compute_idf(num_docs, dfs):
     """Calculate idf."""
-    return np.log(1 + (num_docs - sum_dfs + 0.5) / (sum_dfs + 0.5))
+    return np.sum(np.log(1 + (num_docs - dfs + 0.5) / (dfs + 0.5)))
 
 
 def compute_adj_doc_lens(doc_lens, avg_doc_lens, k1, b):
@@ -55,12 +54,12 @@ def compute_adj_doc_lens(doc_lens, avg_doc_lens, k1, b):
 
 def bm25_similarity(k1: float = 1.2, b: float = 0.75) -> Similarity:
     """BM25 similarity function, as in Lucene 9."""
-    def bm25(term_freqs: np.ndarray, context: ScoringContext) -> np.ndarray:
+    def bm25(term_freqs: np.ndarray, doc_freqs: np.ndarray,
+             context: ScoringContext) -> np.ndarray:
         """Calculate BM25 scores."""
         # Sum doc freqs
-        sum_dfs = np.sum(context.doc_freqs, axis=0)
         # Calculate idf
-        idf = compute_idf(context.num_docs, sum_dfs)
+        idf = compute_idf(context.num_docs, doc_freqs)
         # Calculate tf
         # tf = term_freqs / (term_freqs + k1 * (1 - b + b * doc_lens / avg_doc_lens))
         try:
@@ -69,19 +68,18 @@ def bm25_similarity(k1: float = 1.2, b: float = 0.75) -> Similarity:
             adj_doc_lens = compute_adj_doc_lens(context.doc_lens, context.avg_doc_lens, k1, b)
             context.working["adj_doc_lens"] = adj_doc_lens
         term_freqs /= (term_freqs + adj_doc_lens)
-        return idf * term_freqs
+        result = idf * term_freqs
+        return result
     return bm25
 
 
 def bm25_legacy_similarity(k1: float = 1.2, b: float = 0.75) -> Similarity:
     """BM25 similarity prior to LUCENE-8563 with k1 + 1 in numerator."""
     # (freq * (k1 + 1)) / (freq + k1 * (1 - b + b * fieldLength / avgFieldLength))
-    def bm25(term_freqs: np.ndarray, context: ScoringContext) -> np.ndarray:
+    def bm25(term_freqs: np.ndarray, doc_freqs: np.ndarray, context: ScoringContext) -> np.ndarray:
         """Calculate BM25 scores."""
-        # Sum doc freqs
-        sum_dfs = np.sum(context.doc_freqs, axis=0)
         # Calculate idf
-        idf = np.log(1 + (context.num_docs - sum_dfs + 0.5) / (sum_dfs + 0.5))
+        idf = compute_idf(context.num_docs, doc_freqs)
         # Calculate tf
         tf = (term_freqs * (k1 + 1)) / (term_freqs + k1 * (1 - b + b * context.doc_lens / context.avg_doc_lens))
         return idf * tf
@@ -90,10 +88,10 @@ def bm25_legacy_similarity(k1: float = 1.2, b: float = 0.75) -> Similarity:
 
 def classic_similarity() -> Similarity:
     """Classic Lucene TF-IDF similarity function."""
-    def classic(term_freqs: np.ndarray, context: ScoringContext) -> np.ndarray:
+    def classic(term_freqs: np.ndarray, doc_freqs: np.ndarray, context: ScoringContext) -> np.ndarray:
         """Calculate classic TF-IDF scores."""
         # Sum doc freqs
-        sum_dfs = np.sum(context.doc_freqs, axis=0)
+        sum_dfs = np.sum(doc_freqs, axis=0)
         # Calculate idf as log((docCount+1)/(docFreq+1)) + 1
         idf = np.log((context.num_docs + 1) / (sum_dfs + 1)) + 1
         length_norm = 1.0 / np.sqrt(context.doc_lens)

test/test_solr.py
@@ -209,7 +209,7 @@ def test_edismax(frame, expected, params):
     assert np.allclose(scores, expected)
 
 
-def always_one_similarity(term_freqs: np.ndarray, context: ScoringContext) -> np.ndarray:
+def always_one_similarity(term_freqs: np.ndarray, doc_freqs: np.ndarray, context: ScoringContext) -> np.ndarray:
     term_freqs = term_freqs
     return term_freqs > 0
 

test/test_tmdb.py
@@ -92,8 +92,11 @@ def test_tokenize_tmdb(tmdb_raw_data):
 def test_slice_then_search(tmdb_data):
     star_wars_in_title = tmdb_data['title_tokens'].array.match(["Star", "Wars"])
     star_wars_in_title = tmdb_data[star_wars_in_title]
+    skywalkec_docfreq = star_wars_in_title['overview_tokens'].array.docfreq("Skywalker")
+    assert skywalkec_docfreq <= star_wars_in_title['overview_tokens'].array.corpus_size
     skywalker_bm25 = star_wars_in_title['overview_tokens'].array.score(["Skywalker"])
-    assert skywalker_bm25.shape[0] == 3
+    assert skywalker_bm25.shape[0] == len(star_wars_in_title)
+    assert np.all(skywalker_bm25 >= 0)
 
 
 def test_batch_sizes_give_same(tmdb_data):