a3b10d236e2ddd30d89c7dab10072ae926ec594c
Doug Turnbull <softwaredoug@gmail.com>
2024-04-19 19:34:22 -0400

    v0.0.48

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/guides/single-sourcing-package-version/
-    version="0.0.47",  # Required
+    version="0.0.48",  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
